5f77f6f5a487439fd74b7290fa6066433446e5c8
nik-kita <nik.moiseienko@gmail.com>
2022-03-17 23:03:49 +0200

    feat(ku-ws): update constructor logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-async function main() {
+import { ChannelEnum } from './redis/global-state';
+import { KuRedis } from './redis/ku-redis';
+import { KuWs } from './ws/ku-ws';
 
+async function main() {
+    const i = await KuRedis
+        .generate();
+
+    await KuWs
+        .init();
+    i.on(ChannelEnum.WS_OPEN).do(console.log);
+    await i
+        .to(ChannelEnum.WS_OPEN)
+        .onlyIfStateLike({ 'kuWs.open()': 'not' })
+        .publish({ hello: 'world' });
 }
 
 main().catch((error) => {
--- src/ws/ku-ws.ts
+++ src/ws/ku-ws.ts
@@ -1,6 +1,6 @@
 import qs from 'qs';
 import { v4 } from 'uuid';
-import Ws from 'ws';
+import Ws, { WebSocket as Tws } from 'ws';
 import { KuRequest } from '../api/index.api';
 import { ChannelEnum } from '../redis/global-state';
 import { KuRedis } from '../redis/ku-redis';
@@ -10,12 +10,15 @@ const channels = [
 ];
 
 export class KuWs {
-    private constructor(private kuRedis: KuRedis) {
+    private ws?: Tws;
 
-    }
+    private kuRedis?: KuRedis;
+
+    private constructor() {}
 
     public static async init() {
         const kuRedis = await KuRedis.generate(channels);
+        const kuWs = new KuWs();
 
         kuRedis
             .on(ChannelEnum.WS_OPEN)
@@ -26,12 +29,13 @@ export class KuWs {
                     // TODO check all servers. May be we should choose the best.
                     const [server] = instanceServers;
 
-                    const ws = new Ws(`${server.endpoint}${qs.stringify({ token, id }, { addQueryPrefix: true })}`);
+                    kuWs.ws = new Ws(`${server.endpoint}${qs.stringify({ token, id }, { addQueryPrefix: true })}`);
                     // TODO dev pause
                     console.log('hi');
                 });
             }, { [ChannelEnum.WS_OPEN]: 'yes' });
+        kuWs.kuRedis = kuRedis;
 
-        return new KuWs(kuRedis);
+        return kuWs;
     }
 }
