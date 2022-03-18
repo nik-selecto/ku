85c3b74cb7a6d44db952232e9796f03f63d6552a
nik <nik.moiseienko@gmail.com>
2022-03-19 01:57:38 +0200

    refactor(rm ku-redis and ku-ws): remove all difficult code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-import { ChannelEnum } from './redis/global-state';
-import { KuRedis } from './redis/ku-redis';
-import { pause } from './utils/pause';
-import { KuWs } from './ws/ku-ws';
-
 async function main() {
-    const i = await KuRedis
-        .generate();
 
-    await KuWs
-        .init();
-
-    i.on(ChannelEnum.WS_OPEN, console.log);
-
-    await i
-        .to(ChannelEnum.WS_OPEN)
-        .onlyIfStateLike({ WS: 'closed' })
-        .publish({ hello: 'world' });
-
-    await pause(3);
-    console.log(await i.getState());
-
-    i.to(ChannelEnum.WS_CLOSE)
-        .onlyIfStateLike({ WS: 'open' })
-        .publish({ ok: 'google' });
-
-    console.log('===');
 }
 
 main().catch((error) => {
