ee7aadcbf68114e90d44a9fe26eb93584013cf63
nik <nik.moiseienko@gmail.com>
2022-03-20 13:47:23 +0200

    fix(redis-controller): on('message', (channel, data) => void)!!!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-async function main() {
+import { RedisController } from './redis-controller';
 
+async function main() {
+    const rc = await RedisController.init();
+
+    rc.on({ ws: 'open' }, (state) => {
+        console.log(state);
+    });
+    rc.rewriteState({ ws: 'open' });
 }
 
 main().catch((error) => {
--- src/redis-controller.ts
+++ src/redis-controller.ts
@@ -34,21 +34,23 @@ export class RedisController {
         let updateState = '';
         _pub.get('state').then((state) => {
             updateState = JSON.stringify({ ...JSON.parse(state!), ...data });
-
-            return _pub.set('state', updateState);
+            _pub.set('state', updateState);
         }).then(() => {
             _pub.publish('state', updateState);
         });
     }
 
-    public on(data: Partial<StateType>, cb: (redisController: RedisController, state: StateType) => void) {
+    public on(data: Partial<StateType>, cb: (state: StateType, redisController: RedisController) => void) {
         const entries = Object.entries(data);
 
-        this.sub.on('state', (_data) => {
+        this.sub.on('message', (channel, _data) => {
+            if (channel !== 'state') return;
+
             const dataAsObj = JSON.parse(_data);
+
             if (entries.some(([k, v]) => dataAsObj[k] !== v)) return;
 
-            cb(this, dataAsObj);
+            cb(dataAsObj, this);
         });
     }
 }
